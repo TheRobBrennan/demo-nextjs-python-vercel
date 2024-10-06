f4755eb7a21fa0b5f2443504a5dfbe723c2e734e
Rob Brennan <rob@sploosh.ai>
2024-10-05 19:08:43 -0700

    Add ping endpoint for FastAPI

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
@@ -6,3 +7,10 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.get("/api/py/ping")
+async def ping():
+    return {
+        "message": "Greetings, Earthling! Your ping has reached the cosmos.",
+        "timestamp": datetime.utcnow().isoformat()
+    }
